1e0bff9507aeff19cba39816629eaea52c530158
Dmitrii Tsygankov <Dmitrii.Tsygankov@jetbrains.com>
2024-02-05 16:35:12 +0100

    tried to add test suite

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,6 +1,11 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
 
+@SuiteDisplayName("FirstSuite")
 public class MainClassTest {
     @Test
     public void testHelloWorld() {
